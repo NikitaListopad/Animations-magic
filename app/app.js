9723632af8e223cb5929fc98746ac67795fb0a1c
Mykyta Lystopad <listopad.nikita96@gmail.com>
2022-11-09 19:59:21 +0200

    update observe method

--- app/app.js
+++ app/app.js
@@ -5,11 +5,7 @@ const contactButtonText = 'Contact us';
 (function () {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show')
-            } else {
-                entry.target.classList.remove('show')
-            }
+            entry.target.classList.toggle('show', entry.isIntersecting)
         })
     })
 
